d000561e7941d286abedc1fdce4fa80d448b94fd
LimChaeYeon <86760506+LimChaeYeon1003@users.noreply.github.com>
2022-11-09 11:23:22 +0900

    Update OMU_train.sh

--- Classification_OMU/OMU_train.sh
+++ Classification_OMU/OMU_train.sh
@@ -3,7 +3,7 @@ source /home/dgxadmin/anaconda3/etc/profile.d/conda.sh
 conda activate osj_env
 export CUDA_VISIBLE_DEVICES=0,1,2,3:
 # python ISTA-U-Net-main/ista_unet/train_ct.py #train ISTA-U-NET
-python ISTA-U-Net-main/ista_unet/evaluation.py
+python "$(dirname $0)/"ISTA-U-Net-main/ista_unet/evaluation.py
 # train (noise)
 sh $(dirname $(realpath $0))/1_Codes_pre/run_pre.sh
 sh $(dirname $(realpath $0))/2_Codes_3D/run_train.sh
